e8a138e51886fc2f4a9bbb04f0aeb1c197354c73
TracyAntonia <aantonjuguna@gmail.com>
2023-10-12 16:44:07 +0300

    Completed assignments

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -9,13 +7,12 @@ from models import db, Plant
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+app.json_compact = False  
 
 migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
-
 
 class Plants(Resource):
 
@@ -37,9 +34,7 @@ class Plants(Resource):
 
         return make_response(new_plant.to_dict(), 201)
 
-
 api.add_resource(Plants, '/plants')
-
 
 class PlantByID(Resource):
 
@@ -47,9 +42,32 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id): 
+        data = request.get_json()
+        plant = Plant.query.get(id)
+
+        if not plant:
+            return make_response(jsonify({"message": "Plant not found"}), 404)
+
+        if 'is_in_stock' in data:
+            plant.is_in_stock = data['is_in_stock']
+
+        db.session.commit()
+
+        return make_response(plant.to_dict(), 200)
+
+    def delete(self, id):  
+        plant = Plant.query.get(id)
+
+        if not plant:
+            return make_response('', 204)  
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response('', 204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
